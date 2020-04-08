ce174281d7ddf6e879ddacf140efb3f0857c2b0e
Chintan Sheth <chintz604@gmail.com>
2020-04-09 01:18:41 +0530

    comma missing

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ new L.tileLayer(
 
 //  'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
 
-'https://{s}.tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png' {
+'https://{s}.tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 }).addTo(map);
 
